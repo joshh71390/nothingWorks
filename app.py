8102a9974e27bf2ea4d9bef08847f481cfebaa49
josh hoffer <joshh71390@gmail.com>
2023-04-19 11:50:28 -0400

    commit

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 from flask import Flask
+from beem import Steem
 
 
 app = Flask(__name__)
+steem = Steem("https://api.steemit.com")
 
+@app.route('/sp_to_sbd')
+def sp_to_sbd():
+    _sp = float(request.args.get('sp'))
+    _prs = int(request.args.get('prs'))
+    _vp = int(request.args.get('vp'))
+    _pct = int(request.args.get('pct'))
 
-@app.route("/")
-def index():
-    return "Hello, World!"
+    val = steem.sp_to_sbd(_sp, _prs, _vp, _pct, not_broadcasted_vote=True)
 
+    return str(val)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/sbd_to_vote_pct')
+def sbd_to_vote_pct():
+    _sbd = request.args.get('sbd')
+    _prs = float(request.args.get('prs'))
+    _sp = float(request.args.get('sp'))
+    _vp = float(request.args.get('vp'))
+    _sbd = _sbd + " SBD"
+
+    perc = steem.sbd_to_vote_pct(sbd=_sbd, post_rshares=_prs, steem_power=_sp, vests=None, voting_power=_vp, not_broadcasted_vote=True, use_stored_data=True)
+
+    return str(perc)
+
+if __name__ == '__main__':
+    app.run()
